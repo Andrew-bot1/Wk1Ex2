fe1f03bcb90e16beb136e143cf4d45f2fadfbd3b
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-01-19 19:48:18 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Asks for first name
+Console.WriteLine("What is your first name?:");
+
+// Makes first name a variable
+string fname = Console.ReadLine();
+
+// Asks for last name
+Console.WriteLine("What is your last name?:");
+
+// Makes first name a variable
+string lname = Console.ReadLine();
+
+// Asks for age
+Console.WriteLine("What is your age?:");
+
+// Makes first name a variable
+string age = Console.ReadLine();
+
+// Asks for favorite color
+Console.WriteLine("What is your favorite color?:");
+
+// Makes first name a variable
+string color = Console.ReadLine();
+
+// Prints the variables entered
+Console.WriteLine("Hello, " + fname + " " + lname + "! You are " + age + " years old and your favorite color is " + color + ".");
